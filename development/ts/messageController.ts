ad26def766634ac6e02829273a6a7a32379d3d50
Devin Zygann <devin.zygann@freenet.ag>
2024-08-26 18:33:15 +0200

    added new enums and modifyed the schemas

--- development/ts/messageController.ts
+++ development/ts/messageController.ts
@@ -1,44 +1,54 @@
 /**
+ * An enum to set the single message types.
+ */
+enum MESSAGE_TYPE { TEXT, ATTACHMENT, POLL, EMBETTED }
+
+/**
+ * This enum sets the type for the ATTACHMENT type
+ */
+enum ATTACHMENT_TYPE { IMAGE, VIDEO, STICKER, GIF }
+
+/**
  * This function loads all messages from the channel
  * @param {string} cid 
  */
-function loadMessagesFromChannel(cid:string) {
+function loadMessagesFromChannel(cid: string) {
     fetch(`http://localhost:3000/api/v1/channels/${cid}/messages`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 /**
  * This function loads all members from the channel
  * @param {string} cid 
  */
-function loadMemberFromChannel(cid:string) {
+function loadMemberFromChannel(cid: string) {
     fetch(`http://localhost:3000/api/v1/channels/${cid}/member`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
 
 /**
  * This function loads all channels from the user
  * @param {string} uid 
  */
-function loadChannelsFromUser(uid:string) {
+function loadChannelsFromUser(uid: string) {
     fetch(`http://localhost:3000/api/v1/users/${uid}/channels`)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
 }
--- schemas/message.js
+++ schemas/message.js
@@ -1,12 +1,32 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const MessageContentSchema = new Schema({
+    value: { type: String, required: true },
+    poll: {
+        settings: {
+            onlyOneAnswer: { type: Boolean },
+            timeBasedPoll: { type: Boolean },
+            timeDuration: { type: Date }
+        },
+        title: { type: String },
+        options: []
+    },
+    attachment: {
+        type: { type: Number, enum:[0,1,2,3,4]},
+        attachmentUrl: { type: String },
+        attachmentAlt: { type: String },
+
+    }
+})
+
 const MessageSchema = new Schema({
-    id: {type: String, required: true },
-    content: {type: String, required: true },
-    author: {type: String, required: true },
-    date: {type: Date, required: true },
-    channel: {type: String, required: true },
+    id: { type: String, required: true },
+    type: { type: Number, required: true },
+    content: MessageContentSchema,
+    author: { type: String, required: true },
+    date: { type: Date, required: true },
+    channel: { type: String, required: true },
 });
 
 const Message = mongoose.model('Message', MessageSchema);
