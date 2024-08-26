7a376770845a648d586b25a04246ca8f20d4c8e5
Devin Zygann <devin.zygann@freenet.ag>
2024-08-26 18:12:59 +0200

    added new endpoints

--- routers/channels/channels.js
+++ routers/channels/channels.js
@@ -3,6 +3,19 @@ const express = require('express');
 const { resCode, resJSON } = require('../../functions/response');
 const router = express.Router();
 
+router.get("/:cid", (req, res) => {
+    const { cid } = req.params
+    try {
+        resJSON(res, "success", 200, {
+            cid
+        })
+    } catch (err) {
+        console.error(err);
+        resCode(res, 500)
+
+    }
+});
+
 router.get("/:cid/messages/:mid", (req, res) => {
     const { cid, mid } = req.params;
     try {
@@ -13,6 +26,34 @@ router.get("/:cid/messages/:mid", (req, res) => {
     } catch (err) {
         console.error(err);
         resCode(res, 500);
+    }
+});
+
+router.get("/:cid/member", (req, res) => {
+    const { cid } = req.params
+    try {
+        resJSON(res, "success", 200, {
+            cid,
+            members: []
+        })
+    } catch (err) {
+        console.error(err);
+        resCode(res, 500)
+
+    }
+});
+
+router.get("/:cid/member/:uid", (req, res) => {
+    const { cid, uid } = req.params
+    try {
+        resJSON(res, "success", 200, {
+            cid,
+            uid,
+        })
+    } catch (err) {
+        console.error(err);
+        resCode(res, 500)
+
     }
 });
 
