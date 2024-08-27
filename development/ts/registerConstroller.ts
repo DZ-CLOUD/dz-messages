d8eac74a7db55aa4625169c745198ea9ad53c156
Devin Zygann <devin.zygann@freenet.ag>
2024-08-27 17:00:20 +0200

    added new register form

--- development/ts/registerConstroller.ts
+++ development/ts/registerConstroller.ts
@@ -1,26 +1,65 @@
-enum PAGES {
-    EMAIL,
-    USERNAME,
-    PERSONALIZATION,
-    PASSWORD,
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const steps = document.querySelectorAll<HTMLElement>('.step');
+    const nextButton = document.getElementById('nextButton') as HTMLButtonElement;
+    const backButton = document.getElementById('backButton') as HTMLButtonElement;
+    const submitButton = document.getElementById('submitButton') as HTMLButtonElement;
+    let currentStep = 0;
 
-let currentPage:number = 0;
-function nextPage() {
-    currentPage++
-}
+    function showStep(step: number) {
+        steps.forEach((stepElement, index) => {
+            stepElement.classList.toggle('active', index === step);
+        });
 
-function previousPage() {
-    currentPage--
-}
-
-function pageController() {
-    switch (currentPage) {
-        case 1:
-            
-            break;
-    
-        default:
-            break;
+        backButton.disabled = step === 0;
+        nextButton.style.display = step === steps.length - 1 ? 'none' : 'inline-block';
+        submitButton.style.display = step === steps.length - 1 ? 'inline-block' : 'none';
     }
-}
+
+    nextButton.addEventListener('click', () => {
+        if (currentStep < steps.length - 1) {
+            currentStep++;
+            showStep(currentStep);
+        }
+    });
+
+    backButton.addEventListener('click', () => {
+        if (currentStep > 0) {
+            currentStep--;
+            showStep(currentStep);
+        }
+    });
+
+    const usernameInput = document.getElementById('username') as HTMLInputElement;
+    const usernamePreview = document.getElementById('usernamePreview') as HTMLSpanElement;
+    usernameInput.addEventListener('input', () => {
+        usernamePreview.textContent = usernameInput.value;
+    });
+
+    const imagePreview = document.getElementById('profilePicturePreview') as HTMLImageElement;
+    const profilePicture = document.getElementById('profilePicture') as HTMLInputElement;
+    profilePicture.addEventListener('change', () => {
+        const file = (event.target as HTMLInputElement).files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                imagePreview.src = e.target?.result as string;
+                imagePreview.classList.replace("d-none", "d-block")
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+
+    const passwordInput = document.getElementById('password') as HTMLInputElement;
+    const confirmPasswordInput = document.getElementById('confirmPassword') as HTMLInputElement;
+    passwordInput.addEventListener('input', () => {
+        if (passwordInput.value !== confirmPasswordInput.value) {
+            confirmPasswordInput.setCustomValidity('Passwords do not match');
+        } else {
+            confirmPasswordInput.setCustomValidity('');
+        }
+    });
+
+
+
+    showStep(currentStep);
+});
---
+++ public/js/app.js
@@ -0,0 +1,101 @@
+var MESSAGE_TYPE;
+(function (MESSAGE_TYPE) {
+    MESSAGE_TYPE[MESSAGE_TYPE["TEXT"] = 0] = "TEXT";
+    MESSAGE_TYPE[MESSAGE_TYPE["ATTACHMENT"] = 1] = "ATTACHMENT";
+    MESSAGE_TYPE[MESSAGE_TYPE["POLL"] = 2] = "POLL";
+    MESSAGE_TYPE[MESSAGE_TYPE["EMBETTED"] = 3] = "EMBETTED";
+})(MESSAGE_TYPE || (MESSAGE_TYPE = {}));
+var ATTACHMENT_TYPE;
+(function (ATTACHMENT_TYPE) {
+    ATTACHMENT_TYPE[ATTACHMENT_TYPE["IMAGE"] = 0] = "IMAGE";
+    ATTACHMENT_TYPE[ATTACHMENT_TYPE["VIDEO"] = 1] = "VIDEO";
+    ATTACHMENT_TYPE[ATTACHMENT_TYPE["STICKER"] = 2] = "STICKER";
+    ATTACHMENT_TYPE[ATTACHMENT_TYPE["GIF"] = 3] = "GIF";
+})(ATTACHMENT_TYPE || (ATTACHMENT_TYPE = {}));
+function loadMessagesFromChannel(cid) {
+    fetch(`http://localhost:3000/api/v1/channels/${cid}/messages`)
+        .then(res => res.json())
+        .then(data => {
+        console.log(data);
+    })
+        .catch(err => {
+        console.log(err);
+    });
+}
+function loadMemberFromChannel(cid) {
+    fetch(`http://localhost:3000/api/v1/channels/${cid}/member`)
+        .then(res => res.json())
+        .then(data => {
+        console.log(data);
+    })
+        .catch(err => {
+        console.log(err);
+    });
+}
+function loadChannelsFromUser(uid) {
+    fetch(`http://localhost:3000/api/v1/users/${uid}/channels`)
+        .then(res => res.json())
+        .then(data => {
+        console.log(data);
+    })
+        .catch(err => {
+        console.log(err);
+    });
+}
+document.addEventListener('DOMContentLoaded', () => {
+    const steps = document.querySelectorAll('.step');
+    const nextButton = document.getElementById('nextButton');
+    const backButton = document.getElementById('backButton');
+    const submitButton = document.getElementById('submitButton');
+    let currentStep = 0;
+    function showStep(step) {
+        steps.forEach((stepElement, index) => {
+            stepElement.classList.toggle('active', index === step);
+        });
+        backButton.disabled = step === 0;
+        nextButton.style.display = step === steps.length - 1 ? 'none' : 'inline-block';
+        submitButton.style.display = step === steps.length - 1 ? 'inline-block' : 'none';
+    }
+    nextButton.addEventListener('click', () => {
+        if (currentStep < steps.length - 1) {
+            currentStep++;
+            showStep(currentStep);
+        }
+    });
+    backButton.addEventListener('click', () => {
+        if (currentStep > 0) {
+            currentStep--;
+            showStep(currentStep);
+        }
+    });
+    const usernameInput = document.getElementById('username');
+    const usernamePreview = document.getElementById('usernamePreview');
+    usernameInput.addEventListener('input', () => {
+        usernamePreview.textContent = usernameInput.value;
+    });
+    const imagePreview = document.getElementById('profilePicturePreview');
+    const profilePicture = document.getElementById('profilePicture');
+    profilePicture.addEventListener('change', () => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                imagePreview.src = e.target?.result;
+                imagePreview.classList.replace("d-none", "d-block");
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirmPassword');
+    passwordInput.addEventListener('input', () => {
+        if (passwordInput.value !== confirmPasswordInput.value) {
+            confirmPasswordInput.setCustomValidity('Passwords do not match');
+        }
+        else {
+            confirmPasswordInput.setCustomValidity('');
+        }
+    });
+    showStep(currentStep);
+});
+//# sourceMappingURL=app.js.map
