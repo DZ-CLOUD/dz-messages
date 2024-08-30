973d49d1dda057df2bc93528e58d22a0d1795efe
Devin Zygann <devin.zygann@freenet.ag>
2024-08-30 10:50:33 +0200

    [UPDATE] game

--- schemas/game.js
+++ schemas/game.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
     gid: { type: String, required: true },
+    guild_id: { type: String, ref: "Guild", required: true},
     name: { type: String, required: true },
     icon: { type: String, required: true },
 }, { timestamps: true });
