d856f25950541d2509af7fa5beb4378054c48d20
Devin Zygann <rotermond08@gmail.com>
2024-08-31 23:59:27 +0200

    [UPDATE] updated user.js

--- schemas/user.js
+++ schemas/user.js
@@ -1,74 +1,82 @@
 const mongoose = require('mongoose');
-const { randomHexCode } = require("../functions/random")
+const {randomHexCode} = require("../functions/random")
 const Schema = mongoose.Schema;
 
 const notificationPreferenceSchema = new Schema({
-    type: { type: String, required: true },
-    key: { type: String, required: true },
-    description: { type: String, required: true },
-    value: { type: String, required: true },
-    enabled: { type: Boolean, required: true },
+    type: {type: String, required: true},
+    key: {type: String, required: true},
+    description: {type: String, required: true},
+    value: {type: String, required: true},
+    enabled: {type: Boolean, required: true},
 });
 
 const badgeSchema = new Schema({
-    id: { type: String, required: true},
-    description: { type: String, required: true},
-    icon: { type: String, required: true},
-    link: { type: String, required: true},
+    id: {type: String, required: true},
+    description: {type: String, required: true},
+    icon: {type: String, required: true},
+    link: {type: String, required: true},
 })
 
 const connectionSchema = new Schema({
-    type: { type: String, required: true},
-    id: { type: String, required: true},
-    name: { type: String, required: true},
-    verified: { type: String, default: true},
-    metadata: { type: Schema.Types.Mixed, default: null}
+    friend_sync: {type: Boolean, default: true},
+    type: {type: String, required: true},
+    integrations: {type: Array, default: []},
+    id: {type: String, required: true},
+    name: {type: String, required: true},
+    verified: {type: String, default: true},
+    metadata_visibility: {type: Boolean, default: true},
+    metadata: {type: Schema.Types.Mixed, default: null},
+    revoked: {type: Boolean, default: false},
+    show_activity: {type: Boolean, default: true},
+    two_way_link: {type: Boolean, default: true},
+    visibility: {type: Number, default: 1},
+    access_token: { type: String, default: undefined},
 });
 
 const UserSchema = new Schema({
-    uid: { type: String, required: true },
-    username: { type: String, required: true },
-    display_name: { type: String, required: true },
-    email: { type: String, required: true },
+    uid: {type: String, required: true},
+    username: {type: String, required: true},
+    display_name: {type: String, required: true},
+    email: {type: String, required: true},
     security: {
-        password: { type: String, required: true },
+        password: {type: String, required: true},
     },
-    avatar: { type: String, required: true },
-    avatar_overlay: { type: String, default: null },
-    avatar_decoration_data: { type: Array, default: null},
-    biography: { type: String, default: null },
-    pronouns: { type: String, default: null },
-    banner: { type: String, default: null },
-    banner_color: { type: String, default: randomHexCode },
-    banner_overlay: { type: String, default: null },
-    profile_overlay: { type: String, default: null },
-    accent_color: { type: String, default: randomHexCode },
-    public_flags: { type: Number, default: 0 },
-    status: { type: String, default: 'online' },
-    playStatus: {
-        gameId: { type: String, default: "" },
-        gameStatus: { type: String, default: "" },
-        gameTime: { type: Date, default: Date.now() }
+    avatar: {type: String, required: true},
+    avatar_overlay: {type: String, default: null},
+    avatar_decoration_data: {type: Array, default: null},
+    biography: {type: String, default: null},
+    pronouns: {type: String, default: null},
+    banner: {type: String, default: null},
+    banner_color: {type: String, default: randomHexCode},
+    banner_overlay: {type: String, default: null},
+    profile_overlay: {type: String, default: null},
+    accent_color: {type: String, default: randomHexCode},
+    public_flags: {type: Number, default: 0},
+    status: {type: String, default: 'online'},
+    play_status: {
+        gameId: {type: String, default: ""},
+        gameStatus: {type: String, default: ""},
+        gameTime: {type: Date, default: ""}
     },
     settings: {
-        theme: { type: String, default: 'light' },
-        language: { type: String, default: 'en' },
-        notifications: { type: Boolean, default: true },
-        notification_preferences:[notificationPreferenceSchema],
-        privacy: { type: Boolean, required: true },
-        shareStatus: { type: Boolean, default: true },
-        sharePlayStatus: { type: Boolean, default: true },
+        theme: {type: String, default: 'light'},
+        language: {type: String, default: 'en'},
+        notifications: {type: Boolean, default: true},
+        notification_preferences: [notificationPreferenceSchema],
+        privacy: {type: Boolean, required: true},
+        shareStatus: {type: Boolean, default: true},
+        sharePlayStatus: {type: Boolean, default: true},
     },
-    flags: { type: Number, default: null},
-    bio: { type: String, default: null},
+    flags: {type: Number, default: null},
+    bio: {type: String, default: null},
     connected_accounts: [connectionSchema],
-    premium_since: { type: Date, default: null},
-    premium_type: { type: Number, default: null},
-    premium_guild_since: { type: Date, default: null},
-    profile_themes_experiment_bucket: { type: Number, default: null},
+    premium_since: {type: Date, default: null},
+    premium_type: {type: Number, default: null},
+    premium_guild_since: {type: Date, default: null},
+    profile_themes_experiment_bucket: {type: Number, default: null},
     badges: [badgeSchema],
     guild_badges: [badgeSchema],
-}, { timestamps: true });
+}, {timestamps: true});
 
 const User = mongoose.model('User', UserSchema);
 
